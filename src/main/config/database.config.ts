c6d93160b81bdbf947b5dbc8098dfd72083d540a
David-Chavier <davidlustosa3@gmail.com>
2023-07-27 16:03:06 -0300

    Made the connection to the database and created the models

---
+++ src/main/config/database.config.ts
@@ -0,0 +1,14 @@
+import { DataSource } from "typeorm";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export default new DataSource({
+  type: "postgres",
+  url: process.env.DB_URL,
+  port: 5432,
+  ssl: { rejectUnauthorized: false },
+  migrations: ["src/app/shared/database/migrations/**/*.ts"],
+  entities: ["src/app/shared/database/entities/**/*.ts"],
+  schema: "vagas",
+});
