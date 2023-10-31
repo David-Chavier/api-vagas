66794dcd0e092ae2744e426276ae3d51b2222695
David-Chavier <davidlustosa3@gmail.com>
2023-10-31 15:32:13 -0300

    test: cria os testes unitarios para as rotas de recrutadores

---
+++ tests/app/features/recruiter/usecases/create-recruiter.usecase.spec.ts
@@ -0,0 +1,75 @@
+import { Database } from "../../../../../src/main/database/database.connection";
+import { CacheDatabase } from "../../../../../src/main/database/redis.connection";
+import { CreateRecruiterUsecase } from "../../../../../src/app/features/recruiter/usecases/create-recruiter.usecase";
+import { UserRepository } from "../../../../../src/app/features/user/repositories/user.repository";
+import { UserType } from "../../../../../src/app/models/user-type.model";
+import { User } from "../../../../../src/app/models/user.model";
+
+describe("testando usecase de criação de candidato", () => {
+  beforeAll(async () => {
+    await Database.connect();
+    await CacheDatabase.connect();
+  });
+
+  afterAll(async () => {
+    await Database.connection.destroy();
+    await CacheDatabase.connection.quit();
+  });
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+
+  const createSut = () => {
+    return new CreateRecruiterUsecase();
+  };
+
+  test("Deveria retornar erro 400 se já tiver o email no banco de dados", async () => {
+    const sut = createSut();
+    const user = new User(
+      "any_name",
+      "any_email",
+      "any_password",
+      UserType.Candidate,
+      "any_enterprise"
+    );
+
+    jest.spyOn(UserRepository.prototype, "getByEmail").mockResolvedValue(user);
+
+    const result = await sut.execute({
+      name: "any_name",
+      email: "any_email",
+      password: "any_password",
+      enterpriseName: "any_enterprise",
+    });
+
+    expect(result).toBeDefined();
+    expect(result.code).toBe(400);
+    expect(result.ok).toEqual(false);
+    expect(result).toHaveProperty("code", 400);
+    expect(result.message).toBe("User already exists");
+    expect(result).not.toHaveProperty("data");
+  });
+
+  test("Deveria retornar 200 se o recrutador for criado", async () => {
+    const sut = createSut();
+
+    jest
+      .spyOn(UserRepository.prototype, "getByEmail")
+      .mockResolvedValue(undefined);
+
+    const result = await sut.execute({
+      name: "any_name",
+      email: "any_email",
+      password: "any_password",
+      enterpriseName: "any_enterprise",
+    });
+
+    expect(result).toBeDefined();
+    expect(result.code).toBe(201);
+    expect(result.ok).toEqual(true);
+    expect(result).toHaveProperty("code", 201);
+    expect(result.message).toBe("Recruiter successfully created");
+  });
+});
